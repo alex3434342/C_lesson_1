55ba652c0b9a78b1925cf5049286421ca576c1d7
alex3434342 <fantomgri@mail.ru>
2022-01-17 16:58:29 +0300

    1

--- 1.c
+++ 1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
- int main(void)
+
+int	main(void)
 {
-char c;
-while ((c = getchar()) !=EOF) {
-if (c>='a'&& c<= 'z') putchar (c+'A'-'a');
-else putchar (c);
-}
-return(0);
+	char	c;
+
+	while ((c = getchar()) !=EOF) {
+		if (c>='a'&& c<= 'z') putchar (c+'A'-'a');
+		else putchar (c);
+	}
+	return(0);
 }
 
